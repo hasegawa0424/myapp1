2032d8d85893e92987bb46fbe548431251e8b47e
hasegawa <si19051@edu.aomori-u.ac.jp>
2023-01-04 17:57:49 +0900

    画像表示調整、FirestoreからURLを調達

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
+      appBar: AppBar(title: Text('PortfolioApp')),
       body: FutureBuilder(
         future: initialize(),
         builder: (context, snapshot) {
@@ -62,71 +62,100 @@ class _MyHomePageState extends State<MyHomePage> {
           //     Text(name[1] + ':' + votes[1].toString()),
           //   ],
 
-          return SingleChildScrollView(child:Column(
+          return SingleChildScrollView(
+            child:Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Card(
-                child: Column(
-                  children: [
-                    Ink.image(
-                      image: NetworkImage(
-                        'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
-                      ),
-                      height: 240,
-                      fit: BoxFit.cover,
+                children: [
+                  Card(
+                    child: Column(
+                      children: [
+                        Ink.image(
+                          image: NetworkImage(
+                            documentList[0].get('imageURL').toString(),
+                          ),
+                          height: 240,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                          child: Text(
+                            documentList[0].get('title').toString(),
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                          child: Text(
+                            documentList[0].get('name').toString(),
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                          child: Text(
+                            documentList[0].get('registration date').toString(),
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                          child: Text(
+                            documentList[0].get('introduction').toString(),
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                          child: Text(
+                            documentList[0].get('comment').toString(),
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        'The cat is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.',
-                        style: TextStyle(fontSize: 16),
-                      ),
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        Ink.image(
+                          image: NetworkImage(
+                            'https://www.amgakuin.co.jp/contents/common/image/course/cg-design/top/pc_mainImg1.jpg',
+                          ),
+                          height: 240,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                          child: Text(
+                            'The cat is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: [
-                    Ink.image(
-                      image: NetworkImage(
-                        'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
-                      ),
-                      height: 240,
-                      fit: BoxFit.cover,
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        Ink.image(
+                          image: NetworkImage(
+                            'https://booth.pximg.net/57d05e9d-0336-44a8-8ef7-9e5c58a9f641/i/2994358/a467d0db-3ed5-4a38-a158-4a122ae94d81_base_resized.jpg',
+                          ),
+                          height: 240,
+                          fit: BoxFit.contain,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                          child: Text(
+                            'The cat is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        'The cat is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: [
-                    Ink.image(
-                      image: NetworkImage(
-                        'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
-                      ),
-                      height: 240,
-                      fit: BoxFit.cover,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        'The cat is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+                  ),
+                ],
+            ),
           );
         },
       ),
@@ -141,7 +170,7 @@ class _MyHomePageState extends State<MyHomePage> {
     print("##################################################### initialize()");
     documentList.forEach((elem) {
       print(elem.get('name'));
-      print(elem.get('votes'));
+      print(elem.get('registration date'));
       print(elem.get('comment'));
       print(elem.get('introduction'));
       print(elem.get('title'));
