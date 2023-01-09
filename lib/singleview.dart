62c731dbb7eb25f7c360d4173f5ba7bcf8f22834
hasegawa <si19051@edu.aomori-u.ac.jp>
2023-01-09 18:22:51 +0900

    singleview.dartをFutureからStreamに変更+ボタン作成

--- lib/singleview.dart
+++ lib/singleview.dart
@@ -7,92 +7,103 @@ class SingleView extends StatelessWidget {
   List<DocumentSnapshot> documentList = [];
   int index;
 
-  SingleView(this.index){
-    print("################"+index.toString());
+  SingleView(this.index) {
+    print("################" + index.toString());
   }
+
+  get yuto => null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('SingleView')),
-      body: FutureBuilder(
-        future: initialize(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('baby').snapshots(),
         builder: (context, snapshot) {
-          // if (!snapshot.hasData) {
-          //   return LinearProgressIndicator();
-          // }
+          if (!snapshot.hasData) {
+            return LinearProgressIndicator();
+          }
+          print(
+              "##################################################### initialize()");
+          snapshot.data!.docs.forEach((elem) {
+            print(elem.get('name'));
+            print(elem.get('registration date'));
+            print(elem.get('comment'));
+            print(elem.get('introduction'));
+            print(elem.get('title'));
+          });
+          print(
+              "##################################################### initialize()");
           return SingleChildScrollView(
-            child:Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-               Card(
-                child: Column(
-                  children: [
-                    Ink.image(
-                      image: NetworkImage(
-                        documentList[index].get('imageURL').toString(),
+                Card(
+                  child: Column(
+                    children: [
+                      Ink.image(
+                        image: NetworkImage(
+                          snapshot.data!.docs[index].get('imageURL').toString(),
+                        ),
+                        height: 240,
+                        fit: BoxFit.contain,
                       ),
-                      height: 240,
-                      fit: BoxFit.contain,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        documentList[index].get('title').toString(),
-                        style: TextStyle(fontSize: 16),
+                      Padding(
+                        padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                        child: Text(
+                          snapshot.data!.docs[index].get('title').toString(),
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        documentList[index].get('name').toString(),
-                        style: TextStyle(fontSize: 16),
+                      Padding(
+                        padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                        child: Text(
+                          snapshot.data!.docs[index].get('name').toString(),
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        documentList[index].get('registration date').toString(),
-                        style: TextStyle(fontSize: 16),
+                      Padding(
+                        padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                        child: Text(
+                          snapshot.data!.docs[index]
+                              .get('registration date')
+                              .toString(),
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        documentList[index].get('introduction').toString(),
-                        style: TextStyle(fontSize: 16),
+                      Padding(
+                        padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                        child: Text(
+                          snapshot.data!.docs[index]
+                              .get('introduction')
+                              .toString(),
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16).copyWith(bottom: 0),
-                      child: Text(
-                        documentList[index].get('comment').toString(),
-                        style: TextStyle(fontSize: 16),
+                      Padding(
+                        padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                        child: Text(
+                          snapshot.data!.docs[index].get('comment').toString(),
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ),
-                    ),
-                  ],
+                          TextButton(
+                            onPressed: () {
+                              FirebaseFirestore.instance.collection('baby').doc('XC8iCFmvUsiOeN4Qy3Ef').update(
+                                {
+                                  'name': yuto,
+                                },
+                              );/* ボタンがタップされた時の処理 */ },
+                            child: Text('click here'),
+                          )
+                    ],
+                  ),
                 ),
-              ),
-          ],
-          ),
-    );
+              ],
+            ),
+          );
         },
       ),
     );
-  }
-
-  Future<void> initialize() async {
-    final snapshot = await FirebaseFirestore.instance.collection('baby').get();
-    documentList = snapshot.docs;
-
-    print("##################################################### initialize()");
-    documentList.forEach((elem) {
-      print(elem.get('name'));
-      print(elem.get('registration date'));
-      print(elem.get('comment'));
-      print(elem.get('introduction'));
-      print(elem.get('title'));
-    });
-    print("##################################################### initialize()");
   }
 }
